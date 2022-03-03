8c79aeff3777ac6ecf4e1fad9720d2f7ac3b4a5d
Kamrun Nahar <knahar.kamrun@gmail.com>
2022-03-03 09:52:27 +0600

    navbar modification done

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import { Container, Navbar, Nav, Image, Button } from 'react-bootstrap';
+import { Image, Button } from 'react-bootstrap';
 import LogoSec from '../LogoSec/LogoSec';
 import AboutUs from '../AboutUs/AboutUs';
 import Benefit from '../Benefit/Benefit';
@@ -12,39 +12,64 @@ import Roadmap from '../Roadmap/Roadmap';
 import Contact from '../Contact/Contact';
 import Prices from '../Prices/Prices';
 
+
 const Home = () => {
   return (
     <div>
-      <div className="hero-bg justify-content-center">
-        <Container>
-          <Navbar expand="lg" className="middlenav">
-           <div className="logo-div">
-           <Navbar.Brand href="#home">
-              <Image
-                className="pe-5 "
-                src={require('../../asset/logo.png')}
-                style={{ height: '49px' }}
-              />{' '}
-            </Navbar.Brand>
-           </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto ">
-                <Nav.Link href="#home"  >Home</Nav.Link>
-                <Nav.Link href="#home"  >Home</Nav.Link>
-                <Nav.Link href="#home" >About</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#features" >Product</Nav.Link>
-                <Nav.Link href="#pricing" >Roadmap</Nav.Link>
-                <Nav.Link href="#pricing" >Team</Nav.Link>
-                <Nav.Link href="#pricing" >Token</Nav.Link>
-                <Nav.Link href="#pricing" >Blog</Nav.Link>
-                <Nav.Link href="#pricing" >Contact</Nav.Link>
+      <div className="hero-bg ">
+        <nav class="navbar navbar-expand-lg  ">
+          <div class="container">
+            <a class="navbar-brand" href="/">
+              <Image src={require('../../asset/logo.png')} />
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+                <a class="nav-link" href="/">
+                  Features
+                </a>
+                <a class="nav-link" href="/">
+                  About
+                </a>
+                <a class="nav-link" href="/">
+                  Pricing
+                </a>
+                <a class="nav-link" href="/">
+                  Prduct
+                </a>
+                <a class="nav-link" href="/">
+                  Roadmap
+                </a>
+                <a class="nav-link" href="/">
+                  Token
+                </a>
+                <a class="nav-link" href="/">
+                  Team
+                </a>
+                <a class="nav-link" href="/">
+                  Blog
+                </a>
+                <a class="nav-link" href="/">
+                  Contact
+                </a>
                 <Button className="nav-btn ms-3">Buy Token</Button>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Container>
+              </div>
+            </div>
+          </div>
+        </nav>
 
         {/* Hero section  */}
         <div className="container content-bg">
@@ -57,13 +82,12 @@ const Home = () => {
             </b>
           </h1>
           <p className="text2 my-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Leo ipsum
-            aliquet 
-            turpis elit elit natoque varius eget facilisi.<br /> Amet rhoncus sed
-            nulla tristique
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+            Leo ipsum aliquet turpis elit elit natoque varius eget facilisi.
+            <br /> Amet rhoncus sed nulla tristique
             <br />
-            aenean viverra urna nec. Dapibus nascetur amet <br />lacinia et placerat
-            nibh.
+            aenean viverra urna nec. Dapibus nascetur amet <br />
+            lacinia et placerat nibh.
           </p>
           <div className="d-flex">
             <div className="pe-3">
